33b15f6a21d19a8a9257f5742742f80b27703cc1
zuqingMeng <meng.zuqing62@gmail.com>
2024-09-24 22:26:25 -0700

    getting started on a1

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Web Dev!
-        </p>
+        <p>Welcome to Web APP</p>
         <a
           className="App-link"
           href="https://reactjs.org"
