138d45c7ba4a3db346b8796f1cd6ed9428b8d89b
zuqingMeng <meng.zuqing62@gmail.com>
2024-09-27 02:45:58 -0700

    fix

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -127,6 +127,7 @@
               <img id="wd-starship"
                 width="400px"
               src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+              alt="Starship from StarAdvertiser"
               />
               <br />
               Loading a local image:
