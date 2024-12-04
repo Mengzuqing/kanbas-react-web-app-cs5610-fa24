7f75d6facd9a14f70abeb22d1f3f0e4794b8efa2
zuqingMeng <meng.zuqing62@gmail.com>
2024-12-03 16:14:03 -0800

    update

--- src/Kanbas/Courses/Assignments/AssignmentsControlButtons.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentsControlButtons.tsx
@@ -1,17 +1,10 @@
 import { IoEllipsisVertical } from "react-icons/io5";
 import { BsPlus } from "react-icons/bs";
 
-type AssignmentsControlButtonsProps = {
-    addAssignment?: () => void;
-};
-
-export default function AssignmentsControlButtons({ addAssignment }: AssignmentsControlButtonsProps) {
+export default function AssignmentsControlButtons() {
     return (
         <div className="float-end">
-            <BsPlus 
-                className="fs-4" 
-                onClick={() => addAssignment && addAssignment()} // 当addAssignment存在时触发
-            />
+            <BsPlus className="fs-4"/>
             <IoEllipsisVertical className="fs-4" />
         </div>
     );
--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,44 +1,65 @@
-import { Link, useParams } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+
 export default function CoursesNavigation() {
-  const { cid } = useParams();
-  const { pathname } = useLocation();
-  const links = [
-    { label: "Home", path: `/Kanbas/Courses/${cid}/Home` },
-    { label: "Modules", path: `/Kanbas/Courses/${cid}/Modules` },
-    { label: "Piazza", path: `/Kanbas/Courses/${cid}/Piazza` },
-    { label: "Zoom", path: `/Kanbas/Courses/${cid}/Zoom` },
-    { label: "Assignments", path: `/Kanbas/Courses/${cid}/Assignments` },
-    { label: "Quizzes", path: `/Kanbas/Courses/${cid}/Quizzes` },
-    { label: "Grades", path: `/Kanbas/Courses/${cid}/Grades` },
-    { label: "People", path: `/Kanbas/Courses/${cid}/People` },
-  ];
-  return (
-    <div className="wd list-group rounded-0" id="wd-courses-navigation">
-      {links.map((link) => (
-        <Link
-          key={link.label}
-          to={link.path}
-          className={`list-group-item bg-white
-        ${
-          pathname.includes(link.label)
-            ? "text-black bg-white"
-            : "text-danger bg-white"
-        }`}
-          style={
-            pathname.includes(link.label)
-              ? {
-                  borderLeft: "3px solid black",
-                  borderRight: "none",
-                  borderTop: "none",
-                  borderBottom: "none",
-                }
-              : { border: "none" }
-          }
-        >
-          {link.label}
-        </Link>
-      ))}
-    </div>
-  );
+    const { cid } = useParams();
+    const {pathname} = useLocation();
+    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+
+    return (
+        <div className="wd list-group rounded-0 fs-5 d-none d-md-block" id="wd-courses-navigation">
+           
+            {links.map((link) => (
+                <Link key={link} className={`list-group-item border-0 text-danger
+                ${pathname.includes(link) ? "active text-black": ""}`} 
+                id={`wd-course-${link}-link`} to={`/Kanbas/Courses/${cid}/${link}`}>
+                    {link}</Link>
+            )
+            )}
+        </div>
+    );
 }
+
+// import { Link, useParams } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+// export default function CoursesNavigation() {
+//   const { cid } = useParams();
+//   const { pathname } = useLocation();
+//   const links = [
+//     { label: "Home", path: `/Kanbas/Courses/${cid}/Home` },
+//     { label: "Modules", path: `/Kanbas/Courses/${cid}/Modules` },
+//     { label: "Piazza", path: `/Kanbas/Courses/${cid}/Piazza` },
+//     { label: "Zoom", path: `/Kanbas/Courses/${cid}/Zoom` },
+//     { label: "Assignments", path: `/Kanbas/Courses/${cid}/Assignments` },
+//     { label: "Quizzes", path: `/Kanbas/Courses/${cid}/Quizzes` },
+//     { label: "Grades", path: `/Kanbas/Courses/${cid}/Grades` },
+//     { label: "People", path: `/Kanbas/Courses/${cid}/People` },
+//   ];
+//   return (
+//     <div className="wd list-group rounded-0" id="wd-courses-navigation">
+//       {links.map((link) => (
+//         <Link
+//           key={link.label}
+//           to={link.path}
+//           className={`list-group-item bg-white
+//         ${
+//           pathname.includes(link.label)
+//             ? "text-black bg-white"
+//             : "text-danger bg-white"
+//         }`}
+//           style={
+//             pathname.includes(link.label)
+//               ? {
+//                   borderLeft: "3px solid black",
+//                   borderRight: "none",
+//                   borderTop: "none",
+//                   borderBottom: "none",
+//                 }
+//               : { border: "none" }
+//           }
+//         >
+//           {link.label}
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// }
